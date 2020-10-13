4e48fb3bcec9769e4e484fe13df08e73c04492f5
shawn <desaunders10@gmail.com>
2020-10-13 13:48:19 -0500

    add form feature to add custom tasks

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,11 +1,25 @@
 
-var listEl = document.querySelector(".task-list");
-var addTaskButtonEl = document.querySelector("#save-task");
-addTaskButtonEl.addEventListener("click", createTaskHandler);
+var tasksToDoEl = document.querySelector(".task-list");
+var formEl = document.querySelector("#task-form");
+formEl.addEventListener("submit", createTaskHandler);
 
-function createTaskHandler(){
+function createTaskHandler(event){
+    event.preventDefault();
+    // create list item
     var listItemEl = document.createElement("li");
     listItemEl.className = "task-item";
-    listItemEl.textContent = "This is a new task";
-    listEl.appendChild(listItemEl);
+
+    var taskNameInput = document.querySelector("input[name='task-name']").value;
+    var taskTypeInput = document.querySelector("select[name='task-type']").value;
+    // create div to hold task info and add to list item
+    var taskInfoEl = document.createElement("div");
+    // give it a class name
+    taskInfoEl.className = "task-info";
+    // add HTML content to div
+    taskInfoEl.innerHTML = "<h3 class='task-name'>" + taskNameInput + "</h3><span class='task-type'>" + taskTypeInput + "</span>";
+
+    listItemEl.appendChild(taskInfoEl);
+
+    // add entire list item to list
+    tasksToDoEl.appendChild(listItemEl);
 }
